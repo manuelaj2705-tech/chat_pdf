6c37ba3bd8a55336512fdfebbd5ddc6a88ae4ecf
manuelaj2705-tech <manuelaj.27.05@gmail.com>
2026-04-09 07:53:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.chains.question_answering import load_qa_chain
 import platform
 
 # App title and presentation
-st.title('Generación Aumentada por Recuperación (RAG) 💬')
+st.title('Pregunta sobre tu pdf , Recuperación (RAG) 💬')
 st.write("Versión de Python:", platform.python_version())
 
 # Load and display image
